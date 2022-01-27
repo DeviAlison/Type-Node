e90b90a4053dab1f268d5f83116585a77d10673a
Alison Silva <alisonsilva4170@gmail.com>
2022-01-26 21:30:42 -0300

    Termino da quinta aula, implementados os métodos HTTP

--- src/index.js
+++ src/index.js
@@ -3,10 +3,39 @@ const express = require('express'); // aqui acontece o import do pacote necessá
 const app = express(); // Aqui acontece a criação de uma instância (objeto cujos métodos são definidos pela classe) da classe express
 
 app.get('/projects', (request, response) => { // o '/projects' é equivalente a rota que vai ser acessada; Na arrow function que se segue os request e o response são passados como parâmetros da função
-    return response.json({ // Essa resposta gerada no browser já é considerada um endpoint
-        message: 'Hello World!'
-    });
+    return response.json([ // Essa resposta gerada no browser já é considerada um endpoint
+        'Projeto 1',
+        'Projeto 2',
+        'Projeto 3'
+    ]);
 });
+
+app.post('/projects', (request, response) => {
+    return response.json([ // Essa resposta gerada no browser já é considerada um endpoint
+        'Projeto 1',
+        'Projeto 2',
+        'Projeto 3',
+        'Projeto 4'
+    ]);
+})
+
+app.put('/projects', (request, response) => {
+    return response.json([ // Essa resposta gerada no browser já é considerada um endpoint
+        'Projeto 5',
+        'Projeto 2',
+        'Projeto 3',
+        'Projeto 4'
+    ]);
+})
+ 
+app.delete('/projects', (request, response) => {
+    return response.json([ // Essa resposta gerada no browser já é considerada um endpoint
+        'Projeto 5',
+        'Projeto 2',
+        'Projeto 4'
+    ]);
+})
+ 
 
 app.listen(3545, () => {
     console.log('Backend started!'); //mensagem que aparece no terminal quando o servidor for iniciado
