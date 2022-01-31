094f4e53845d2726d6a9e21feb95101e48f4a7dc
Alison Silva <alisonsilva4170@gmail.com>
2022-01-31 06:01:06 -0300

    Aula 7 - (meio da aula)Iniciando desenvolvimento de API Rest

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const express = require('express'); // aqui acontece o import do pacote necessário nessa parte
+const {uuid} = require('uuidv4');
 
 const app = express(); // Aqui acontece a criação de uma instância (objeto cujos métodos são definidos pela classe) da classe express
 
@@ -6,29 +7,26 @@ app.use(express.json()); // necessário definir que o padrão de utilização se
 
 // http://localhost:3545/projects?title=Node&owner=StephenKing
 
-app.get('/projects', (request, response) => { // o '/projects' é equivalente a rota que vai ser acessada; Na arrow function que se segue os request e o response são passados como parâmetros da função
-    const { title } = request.query; // dessa maneira, pegarei o title=Batata da url apenas, mesmo que tenham mais chaves e valor
-    // const query = request.query;  // dessa maneira eu vou pegar os parametros passados na url
+const projects = []; 
 
-    console.log(title);
-    return response.json([ // Essa resposta gerada no browser já é considerada um endpoint
-        'Projeto 1',
-        'Projeto 2',
-        'Projeto 3'
-    ]);
+app.get('/projects', (request, response) => { // o '/projects' é equivalente a rota que vai ser acessada; Na arrow function que se segue os request e o response são passados como parâmetros da função
+    return response.json(projects);
 });
 
 app.post('/projects', (request, response) => {
-    const body = request.body; //necessário configurar o app.use para json
+    const { title, owner } = request.body;
 
-    console.log(body);
+    const id = uuid();
 
-    return response.json([ // Essa resposta gerada no browser já é considerada um endpoint
-        'Projeto 1',
-        'Projeto 2',
-        'Projeto 3',
-        'Projeto 4'
-    ]);
+    const project = {
+        id,
+        title,
+        owner
+    };
+
+    projects.push(project);
+
+    return response.json(project);
 })
 
 app.put('/projects/:id/owner/:owner_id', (request, response) => {
